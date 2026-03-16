930b201163ee571251e987e63c24e5b08e2c5fd7
Sapphire-Nova <thampton8817@gmail.com>
2026-03-15 21:55:38 -0700

    Full Library with Zodiac, Herbs, Crystals and S3 images

---
+++ src/libraryData.js
@@ -0,0 +1,18 @@
+﻿export const herbData = [
+  { name: "Basil", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/basilherb/basil.png", traits: "Abundance, Love, Protection", planet: "Mars" },
+  { name: "Bay Laurel", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/bayleaf/baylaurel.png", traits: "Success, Psychic Power, Vision", planet: "Sun" },
+  { name: "Belladonna", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/belladonnaplant/belladonna.png", traits: "Shadow Work, Protection, Visionary", planet: "Saturn" },
+  { name: "Lavender", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/lavender/Untitled.png", traits: "Peace, Sleep, Purification", planet: "Mercury" },
+  { name: "Mugwort", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/mugwortherb/mugwort.png", traits: "Lucid Dreaming, Intuition", planet: "Moon" },
+  { name: "Sage", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Herbs/sage/Untitled.png", traits: "Wisdom, Cleansing, Longevity", planet: "Jupiter" }
+  // I will continue adding the rest of your 40+ herbs to the cloud database!
+];
+
+export const crystalData = [
+  { name: "Aquamarine", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/aquamarine/macro-mineral-stone-aquamarine-black-background_254845-6375.avif", traits: "Courage, Calm, Communication", chakra: "Throat" },
+  { name: "Black Obsidian", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/black+obsidian/1.webp", traits: "Shielding, Grounding, Truth", chakra: "Root" },
+  { name: "Carnelian", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/carnelian/images.jpg", traits: "Creativity, Vitality, Action", chakra: "Sacral" },
+  { name: "Citrine", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/citrine/360_F_1171493078_fQmaVIe4YwqMZsUl57lPUlUE2vvKqwbc.jpg", traits: "Prosperity, Joy, Energy", chakra: "Solar Plexus" },
+  { name: "Rose Quartz", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/rose+quartz/rose-quartz-crystals-isolate-black-background_332246-47.avif", traits: "Unconditional Love, Healing", chakra: "Heart" },
+  { name: "Selenite", image: "https://crystalphotosforveya.s3.us-east-2.amazonaws.com/Crystals/selenite/lamp-raw-selenite-mineral-double-spiral-lamp-air-purifying-home-decor-146343.webp", traits: "Clarity, High Vibration, Charging", chakra: "Crown" }
+];
